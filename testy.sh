3e45c2c978e8a1f9348698c202afeed4d5af8b63
aA_GitHub <101359728+aA-GitHub1@users.noreply.github.com>
2024-11-14 18:44:56 +0100

    Add files via upload

---
+++ testy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+g++ -std=c++11 ./wzor.cpp -o wzor
+g++ -std=c++11 ./gen.cpp -o gen
+g++ -std=c++11 ./brut.cpp -o brut
+for((i=1;;i++))
+do
+
+  ./gen $i > ./tests/test_$i.in
+  ./wzor < ./tests/test_$i.in > ./tests/wzor_$i.out
+  ./wzor < ./tests/test_$i.in > ./tests/brut_$i.out
+   if ! diff -bq ./tests/wzor_$i.out ./tests/brut_$i.out; then
+    echo "different"
+    break
+    fi
+done
